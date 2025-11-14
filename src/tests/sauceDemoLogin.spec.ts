79b3b68c95d9ed460cbe10b1a8f2365ed815ee43
Abir Aftab <abiraftab@gmail.com>
2025-11-14 11:09:23 -0500

    Added Axe-core accessibility Check

--- src/tests/sauceDemoLogin.spec.ts
+++ src/tests/sauceDemoLogin.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/sauceLoginPage';
-
+// Importing crytojs util to decrypt user name and password
 import { decrypt } from "../utils/cryptojsUtil.ts";
+// Axe-Code Playwright for Accessiblity checks
+const AxeBuilder = require('@axe-core/playwright').default;
 
 test('test', async ({ page }) => {
     // Declare loginPage
@@ -18,6 +20,18 @@ test('test', async ({ page }) => {
     const homePage = await loginPage.clickLoginButton();
     // Verify Home Page Heading
     await homePage.expectHomepageHeadingToBeVisible();
+
+    await test.step('Check Accessibility', async () => {
+        const makeAxeBuilder = () => new AxeBuilder({ page })
+            .withTags(['wcag21a', 'wcag21aa', 'wcag21aaa'])
+            .analyze();
+        const accessibilityScanResults = await makeAxeBuilder(page);
+        // Assertion
+        expect(accessibilityScanResults.violations).toEqual([]);
+    })
+
+
+
     // Open Menu option
     await homePage.navigateToMenu();
     // Click Logout option
